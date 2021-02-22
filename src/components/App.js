e89e0b6f835552a95eb292543bf36f7a78d5b245
Thomas McErlean <tjmcerlean@googlemail.com>
2021-02-22 21:48:36 +0000

    remove sass files

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/style.css';
+
+const App = () => {
+
+    return (
+        <div className='test-container'>
+            Testing
+        </div>
+    );
+}
+
+export default App;
